7ca088c9e48d2d495c399c8c1900b7b1a6e3a931
Nojus0 <thealextgm@gmail.com>
2021-11-23 16:22:25 +0000

    shit

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -4,34 +4,30 @@ import Redis from "ioredis";
 import _ from "lodash";
 import mongoose from "mongoose";
 import typeDefs from "./graphql/typeDefs";
-import { ApolloServer } from "apollo-server-express";
+import { ApolloServer } from "apollo-server-lambda";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 import resolvers from "./graphql/resolvers";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { applyMiddleware } from "graphql-middleware";
 import middlewares from "./graphql/middleware";
 
-(async () => {
-  if (!process.env.MONGO_URI || !process.env.REDIS_URI)
-    throw new Error("Redis or Mongo server uri not found.");
+if (!process.env.MONGO_URI || !process.env.REDIS_URI)
+  throw new Error("Redis or Mongo server uri not found.");
 
-  const app = express();
-  const mongo = await mongoose.connect(process.env.MONGO_URI);
-  const redis = new Redis(process.env.REDIS_URI);
+const app = express();
+const mongo = mongoose.connect(process.env.MONGO_URI);
+const redis = new Redis(process.env.REDIS_URI);
 
-  const schema = makeExecutableSchema({
-    typeDefs,
-    resolvers,
-  });
-  
-  const schemaWithMiddleware = applyMiddleware(schema, ...middlewares);
-  const GQL_SERVER = new ApolloServer({
-    schema: schemaWithMiddleware,
-    context: ({ req, res }) => ({ req, res, redis, mongo }),
-    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
-  });
-  await GQL_SERVER.start();
-  GQL_SERVER.applyMiddleware({ app });
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
 
-  app.listen(4000, () => console.log(`Listening on http://localhost:4000/`));
-})();
+const schemaWithMiddleware = applyMiddleware(schema, ...middlewares);
+const GQL_SERVER = new ApolloServer({
+  schema: schemaWithMiddleware,
+  context: ({ express: { req, res } }) => ({ req, res, redis, mongo }),
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
+
+export const handler = GQL_SERVER.createHandler();
