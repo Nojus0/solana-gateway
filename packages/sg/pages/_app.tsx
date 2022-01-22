1249bb9992d388fadda44268c77dd0226c9181e8
Nojus0 <thealextgm@gmail.com>
2022-01-22 13:01:35 +0000

    Add Google Analytics and Start updating Docs

--- packages/sg/pages/_app.tsx
+++ packages/sg/pages/_app.tsx
@@ -4,14 +4,32 @@ import { Provider, useDispatch } from "react-redux"
 import store from "../src/redux/store"
 import { AnimatePresence } from "framer-motion"
 import currentUserThunk from "../src/redux/thunks/currentUser"
+import Script from "next/script"
 function MyApp({ Component, pageProps }: AppProps) {
-
   return (
-    <Provider store={store}>
-      <AnimatePresence>
-        <Component {...pageProps} />
-      </AnimatePresence>
-    </Provider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA}', { page_path: window.location.pathname });
+          `
+        }}
+      />
+      <Provider store={store}>
+        <AnimatePresence>
+          <Component {...pageProps} />
+        </AnimatePresence>
+      </Provider>
+    </>
   )
 }
 
